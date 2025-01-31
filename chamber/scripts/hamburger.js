8c8cfb8a41ae6beda1ee33ec120b45385e3e3c0f
Cassidy Whitehead <whi21049@byui.edu>
2025-01-31 02:27:06 -0700

    lots of updates and work on chamber! (nav and hero)

---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,10 @@
+// chamber
+// declare const
+// one selects a class of ".navigation", one selects id of "#menu"
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
