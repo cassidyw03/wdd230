4e2c6e3b82a9bb04db70f6962e667540ea52dbc2
Cassidy Whitehead <whi21049@byui.edu>
2025-02-27 20:34:23 -0700

    form assignemnt for course homepage

---
+++ scripts/form.js
@@ -0,0 +1,30 @@
+const p1 = document.querySelector("#password");
+const p2 = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+p2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+	if (p1.value !== p2.value) {
+		message.textContent = "Passwords do not match! Please enter the same password.";
+		message.style.visibility = "show";
+		p2.style.backgroundColor = "#fff0f3";
+		p2.value = "";
+		p2.focus();
+	} else {
+		message.style.display = "none";
+		// p2.style.backgroundColor = "#fff";
+		// p2.style.color = "#000";
+	}
+}
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
