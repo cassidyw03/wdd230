34b05cc7d2d817ae43f216379c01d35527a36c15
Cassidy Whitehead <whi21049@byui.edu>
2025-04-09 21:53:22 -0600

    saving some big changes!

--- scoots/scripts/weather.js
+++ scoots/scripts/weather.js
@@ -1,87 +1,10 @@
-// const currentTemp = document.querySelector("#temperature");
-// const weatherIcon = document.querySelector("#weather-icon");
-// const captionDesc = document.querySelector("figcaption");
-// const maxTemp = document.querySelector("#max-temp");
-
-// const apiKey = "16a94b964db35ab030e90067886290d9";
-// const lat = 20.424876424869062;
-// const lon = -86.928165658883;
-
-// // Cozumel
-// // 20.424876424869062, -86.928165658883
-
-// const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-// const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-
-
-// async function apiFetch() {
-//     try {
-//         // Fetch current weather
-//         const weatherResponse = await fetch(url);
-//         if (!weatherResponse.ok) throw new Error(await weatherResponse.text());
-//         const weatherData = await weatherResponse.json();
-//         console.log("Current Weather Data:", weatherData);
-//         displayResults(weatherData);
-
-//         // Fetch 3-day forecast
-//         const forecastResponse = await fetch(forecastUrl);
-//         if (!forecastResponse.ok) throw new Error(await forecastResponse.text());
-//         const forecastData = await forecastResponse.json();
-//         console.log("Forecast Data:", forecastData);
-//         displayForecast(forecastData);
-
-//     } catch (error) {
-//         console.error("Error fetching weather data:", error);
-//     }
-// }
-
-// function displayResults(data) {
-//     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-
-//     const iconCode = data.weather[0].icon;
-//     const description = data.weather[0].description;
-
-//     weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${iconCode}.png`);
-//     weatherIcon.setAttribute("alt", description);
-//     captionDesc.textContent = description;
-
-//     maxTemp.innerHTML = `${data.main.temp_max}&deg;F`;
-// }
-
-// function displayForecast(data) {
-//     const forecastContainer = document.querySelector("#forecast");
-//     forecastContainer.innerHTML = "";
-
-//     const dailyForecasts = data.list.filter(forecast => forecast.dt_txt.includes("15:00:00"));
-
-//     for (let i = 0; i < dailyForecasts.length; i++) {
-//         const dayData = dailyForecasts[i];
-//         const temp = dayData.main.temp;
-//         const iconCode = dayData.weather[0].icon;
-//         const description = dayData.weather[0].description;
-    
-//         const forecastItem = document.createElement("div");
-//         forecastItem.classList.add("forecast-item");
-    
-//         forecastItem.innerHTML = `
-//             <p>Day ${i + 1}: ${temp}&deg;F</p>
-//             <img src="https://openweathermap.org/img/wn/${iconCode}.png" alt="${description}">
-//             <p>${description}</p>
-//         `;
-    
-//         forecastContainer.appendChild(forecastItem);
-//     }
-    
-// }
-
-// apiFetch();
-
-
-
 const currentTemp = document.querySelector("#temperature");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 const maxTemp = document.querySelector("#max-temp");
+const humidity = document.querySelector("#humidity");
+
+
 
 const apiKey = "16a94b964db35ab030e90067886290d9";
 const lat = 20.424876424869062;
@@ -103,7 +26,7 @@ async function apiFetch() {
         const forecastResponse = await fetch(forecastUrl);
         if (!forecastResponse.ok) throw new Error(await forecastResponse.text());
         const forecastData = await forecastResponse.json();
-        console.log("Forecast Data:", forecastData);
+        // console.log("Forecast Data:", forecastData);
         displayForecast(forecastData);
 
     } catch (error) {
@@ -113,13 +36,14 @@ async function apiFetch() {
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    humidity.textContent = `${data.main.humidity}%`;
 
     const iconCode = data.weather[0].icon;
-    const description = data.weather[0].description;
+    const weatherDetails = data.weather.map(w => `${w.main} - ${w.description}`).join(", ");
 
     weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${iconCode}.png`);
-    weatherIcon.setAttribute("alt", description);
-    captionDesc.textContent = description;
+    weatherIcon.setAttribute("alt", weatherDetails);
+    document.querySelector("#current-desc").textContent = weatherDetails;
 
     maxTemp.innerHTML = `${data.main.temp_max}&deg;F`;
 }
@@ -128,26 +52,27 @@ function displayForecast(data) {
     const forecastContainer = document.querySelector("#forecast");
     forecastContainer.innerHTML = "";
 
-    const dailyForecasts = data.list.filter(forecast => forecast.dt_txt.includes("15:00:00"));
+    const forecastAtThree = data.list.find(forecast => forecast.dt_txt.includes("15:00:00"));
 
-    for (let i = 0; i < dailyForecasts.length; i++) {
-        const dayData = dailyForecasts[i];
-        const temp = dayData.main.temp;
-        const iconCode = dayData.weather[0].icon;
-        const description = dayData.weather[0].description;
+    if (forecastAtThree) {
+        const temp = forecastAtThree.main.temp;
+        const iconCode = forecastAtThree.weather[0].icon;
+        const forecastDetails = forecastAtThree.weather.map(w => `${w.main} - ${w.description}`).join(", ");
 
         const forecastItem = document.createElement("div");
-        forecastItem.classList.add("forecast-item");
+        forecastItem.classList.add("forecast-item", "weather-box");
 
         forecastItem.innerHTML = `
-            <p>Day ${i + 1}: ${temp}&deg;F</p>
-            <img src="https://openweathermap.org/img/wn/${iconCode}.png" alt="${description}">
-            <p>${description}</p>
+        <p class="temp">${temp}&deg;F</p>
+        <img src="https://openweathermap.org/img/wn/${iconCode}.png" alt="${forecastDetails}" class="weather-icon">
+        <figcaption>${forecastDetails}</figcaption>
         `;
+
 
         forecastContainer.appendChild(forecastItem);
     }
 }
+
 
 // Add event listener to close the banner when the "✕" is clicked
 const closeBanner = document.querySelector("#close-banner");
